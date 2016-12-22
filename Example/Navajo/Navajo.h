9f9607641bdc7d27b46a8edfa6b190d0dd88b4b9
François-Julien Alcaraz <fj@mokriya.com>
2016-12-22 14:05:45 +0100

    add framework for carthage support

---
+++ Example/Navajo/Navajo.h
@@ -0,0 +1,19 @@
+//
+//  Navajo.h
+//  Navajo
+//
+//  Created by Francois-julienX Alcaraz on 22/12/16.
+//  Copyright © 2016 Mattt Thompson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Navajo.
+FOUNDATION_EXPORT double NavajoVersionNumber;
+
+//! Project version string for Navajo.
+FOUNDATION_EXPORT const unsigned char NavajoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Navajo/PublicHeader.h>
+
+
